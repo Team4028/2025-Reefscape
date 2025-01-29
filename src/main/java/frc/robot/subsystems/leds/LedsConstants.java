6f949a395d8b1bb1a4e4a7f033408f55fdb344f1
sponge254 <g.stuk@outlook.com>
2025-01-29 13:15:52 -0500

    Added Leds subsystem

---
+++ src/main/java/frc/robot/subsystems/leds/LedsConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.leds;
+
+public class LedsConstants {
+    public static final int NUM_LEDS = 8;
+
+    public static final class Candle {
+        public static final int CAN_ID = 0;
+        public static final String CANBUS = "rio";
+        public static final double BRIGHTNESS_FACTOR = 0.5;
+    }
+}
