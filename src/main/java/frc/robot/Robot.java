3e7e82d4e8c7b9cea886fe9b2753ac0aed1de355
Gabe <gabe.seq@gmail.com>
2025-02-01 15:54:50 -0500

    color sensor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.reduxrobotics.canand.CanandEventLoop;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -26,6 +28,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    CanandEventLoop.getInstance();
   }
 
   /**
