dcacc66932c613833e40469be0291b82b00c1d06
AndrewG3343 <ghabrial.andy@gmail.com>
2025-02-03 20:31:37 -0500

    initial commit

---
+++ src/main/java/frc/robot/subsystems/algae/AlgaeManipulatorConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.algae;
+
+public class AlgaeManipulatorConstants {
+    public static final class TalonSRX {
+        public static final int CAN_ID = 0;
+        public static final  boolean INVERT = false;
+    }
+}
