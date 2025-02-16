05e8710bcdf63b51f8d27a70a762bad042a45cb6
ETHendrix <ezekielhendrix@me.com>
2025-02-15 20:58:32 -0500

    removed singular follower motor for Nick

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -17,69 +16,53 @@ import frc.robot.util.GetMotorData;
 
 public class ElevatorIOTalonFX implements ElevatorIO {
     private final TalonFX leader = new TalonFX(ElevatorConstants.TalonFX.LEADER_ID);
-    private final TalonFX follower = new TalonFX(ElevatorConstants.TalonFX.FOLLOWER_ID);
     private final StatusSignal<Angle> positionRotLeader = leader.getPosition();
     private final StatusSignal<AngularVelocity> velocityRotPerSecLeader = leader.getVelocity();
     private final StatusSignal<AngularAcceleration> accelerationRotPerSecPerSecLeader = leader.getAcceleration();
     private final StatusSignal<Voltage> appliedVoltageLeader = leader.getMotorVoltage();
     private final StatusSignal<Current> currentAmpsLeader = leader.getSupplyCurrent();
-    private final StatusSignal<Angle> positionRotFollower = follower.getPosition();
-    private final StatusSignal<AngularVelocity> velocityRotPerSecFollower = follower.getVelocity();
-    private final StatusSignal<AngularAcceleration> accelerationRotPerSecPerSecFollower = follower.getAcceleration();
-    private final StatusSignal<Voltage> appliedVoltageFollower = follower.getMotorVoltage();
-    private final StatusSignal<Current> currentAmpsFollower = follower.getSupplyCurrent();
+
+    ;
 
     private final MotionMagicVoltage pidControl = new MotionMagicVoltage(0).withSlot(0);
     private final VoltageOut voltageControl = new VoltageOut(0).withEnableFOC(ElevatorConstants.USE_FOC);
     private final DutyCycleOut vbusControl = new DutyCycleOut(0).withEnableFOC(ElevatorConstants.USE_FOC);
-    private final StrictFollower followerControl = new StrictFollower(ElevatorConstants.TalonFX.LEADER_ID);
 
     public ElevatorIOTalonFX() {
         leader.getConfigurator().apply(ElevatorConstants.TalonFX.leaderConfigs);
-        follower.getConfigurator().apply(ElevatorConstants.TalonFX.followerConfigs);
+
         leader.getConfigurator().apply(ElevatorConstants.TalonFX.pidConfigs);
         leader.getConfigurator().apply(ElevatorConstants.TalonFX.mmConfigs);
         leader.getConfigurator().apply(ElevatorConstants.TalonFX.softLimits);
-        follower.getConfigurator().apply(ElevatorConstants.TalonFX.softLimits);
+
     }
 
     @Override
     public void updateInputs(ElevatorIOInputs inputs) {
-        BaseStatusSignal.refreshAll(positionRotLeader, positionRotFollower, velocityRotPerSecLeader,
-                velocityRotPerSecFollower, appliedVoltageLeader, appliedVoltageFollower, currentAmpsLeader,
-                currentAmpsFollower);
-
+        BaseStatusSignal.refreshAll(positionRotLeader, velocityRotPerSecLeader, appliedVoltageLeader,
+                currentAmpsLeader);
         inputs.leaderPosition = positionRotLeader.getValueAsDouble();
-        inputs.followerPosition = positionRotFollower.getValueAsDouble();
         inputs.leaderVelocity = velocityRotPerSecLeader.getValueAsDouble();
-        inputs.followerVelocity = velocityRotPerSecFollower.getValueAsDouble();
         inputs.leaderAcceleration = accelerationRotPerSecPerSecLeader.getValueAsDouble();
-        inputs.followerAcceleration = accelerationRotPerSecPerSecFollower.getValueAsDouble();
         inputs.leaderAppliedVolts = appliedVoltageLeader.getValueAsDouble();
-        inputs.followerAppliedVolts = appliedVoltageFollower.getValueAsDouble();
         inputs.leaderCurrentAmps = currentAmpsLeader.getValueAsDouble();
-        inputs.followerCurrentAmps = currentAmpsFollower.getValueAsDouble();
         inputs.elevatorPositionInches = inputs.leaderPosition * ElevatorConstants.ROT_TO_IN;
         inputs.elevatorVelocityInchesPerSecond = inputs.leaderVelocity * ElevatorConstants.ROT_TO_IN;
         inputs.leaderData = GetMotorData.getTalonFXData(leader);
-        inputs.followerData = GetMotorData.getTalonFXData(follower);
     }
-    
+
     @Override
     public void setVoltage(double volts) {
         leader.setControl(voltageControl.withOutput(volts));
-        follower.setControl(followerControl);
     }
 
     @Override
     public void setVbus(double vBus) {
         leader.setControl(vbusControl.withOutput(vBus));
-        follower.setControl(followerControl);
     }
 
     @Override
     public void setPid(double positionInches) {
         leader.setControl(pidControl.withPosition(positionInches / ElevatorConstants.ROT_TO_IN));
-        follower.setControl(followerControl);
     }
 }
